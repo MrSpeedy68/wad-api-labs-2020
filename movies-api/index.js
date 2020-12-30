2b8e2dae577f06d3cdde8de0c64eec81c7fe6ee0
Adrian Hebel <47033681+MrSpeedy68@users.noreply.github.com>
2020-12-30 13:57:50 +0000

    added routing for add,update,delete movie

--- movies-api/index.js
+++ movies-api/index.js
@@ -1,12 +1,18 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import bodyParser from 'body-parser';
 import moviesRouter from './api/movies';
+
 
 dotenv.config();
 
 const app = express();
 
 const port = process.env.PORT;
+
+//configure body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 
 app.use(express.static('public'));
 app.use('/api/movies', moviesRouter);
